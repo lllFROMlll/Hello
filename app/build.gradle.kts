9fd048b9ffdc93f580e9993c0722551dc3b4d291
lllFROMlll <thiagofrom@gmail.com>
2026-08-01 23:32:33 -0300

    Add Room and KSP dependencies to build.gradle.kts
    Added Room dependencies and KSP plugin for annotation processing.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -40,4 +41,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
 }
